80ce67516e2cf19a6ed580be1065eb0a8df778f9
Rawinder Singh <72056829+lilwindax@users.noreply.github.com>
2022-11-24 22:02:26 +1300

    Added comments

--- Email.py
+++ Email.py
@@ -1,38 +1,21 @@
-# # import packages
-# # below packages are built-in - no need to install anything new!
-# # yupi :)
-# import smtplib
-# from email.message import EmailMessage
-#
-# # set your email and password
-# # please use App Password
-# email_address = "testtargetemail121362472357@gmail.com"
-# email_password = "pabumnvgcdzhipsy"
-#
-# # create email
-# msg = EmailMessage()
-# msg['Subject'] = "Email subject"
-# msg['From'] = email_address
-# msg['To'] = "rawinder457@gmail.com"
-# msg.set_content("This is eamil message")
-#
-# # send email
-# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#     smtp.login(email_address, email_password)
-#     smtp.send_message(msg)
-#
+# Library includes 
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+# Function to send emails 
 def Send_Email(email_address, detection_image):
+    
+    # Variable initialization 
     fromaddr = "testtargetemail121362472357@gmail.com"
     toaddr = email_address
-
+    
+    # Initialize MINEMutilpart 
     msg = MIMEMultipart()
-
+    
+    # Configure email 
     msg['From'] = fromaddr
     msg['To'] = toaddr
     msg['Subject'] = "Detection Notification"
@@ -40,17 +23,20 @@ def Send_Email(email_address, detection_image):
     body = "Dear user there has been a detection. Please see attachment below: "
 
     msg.attach(MIMEText(body, 'plain'))
-
+    
+    # Load detected image as attachment to email 
     filename = detection_image
     attachment = open(detection_image, "rb")
-
+    
+    # Configuration 
     part = MIMEBase('application', 'octet-stream')
     part.set_payload((attachment).read())
     encoders.encode_base64(part)
     part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
+    
     msg.attach(part)
-
+        
+    # Send email    
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(fromaddr, "pabumnvgcdzhipsy")
